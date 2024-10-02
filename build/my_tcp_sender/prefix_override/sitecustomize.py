a2798ea55ceab863400e790901ce64857f562722
samar <167778198+99332365@users.noreply.github.com>
2024-10-02 13:43:42 +0100

    Delete build directory

--- build/my_tcp_sender/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/samar/my_workspace/install/my_tcp_sender'
--- build/my_tcp_sender/share/my_tcp_sender/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/samar/my_workspace/build/my_tcp_sender"
